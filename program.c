87fe2edc784a1ddf03211faafed550534bdd349e
Sana_Shabeel <sanastrobarre99@gmail.com>
2024-02-16 00:01:35 +0300

    changed something

--- program.c
+++ program.c
@@ -1,6 +1,6 @@
 #include <stdio.h>
 
 int main() {
-    printf("Just added a shell function in my shell's configuration file !\n");
+    printf("Just added a shell function in my shell's configuration file change !\n");
     return 0;
 }
